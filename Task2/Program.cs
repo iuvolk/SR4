d59e673d12234817e37fded9deff9a1da7e36483
volkt <volkt@DESKTOP-OUU1E93>
2022-10-27 23:20:46 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность чисел");
+            int n;
+            int i1 = 1;
+            int i2 = 1;
+            int s1 = 0;
+            int s2 = 0;
+
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n < 0)
+                    s1 = i1++;
+                else if (n > 0)
+                    s2 = i2++;
+                else continue;
+            } while (n != 0);
+
+            Console.WriteLine("Количество положительных {0}, количество отрицательных {1}", s2, s1);
+            if (s2>s1)
+                Console.WriteLine("Количество положительных чисел больше");
+            else
+                Console.WriteLine("Количество отрицательных чисел больше");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа A, B, C");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int g = 0;
+            int v = 0;
+            int i1 = 0;
+            int i2 = 0;
+
+            while (a >= c)
+            {
+                a -= c;
+                g++;
+            }
+
+            while (b >= c)
+            {
+                b -= c;
+                v++;
+            }
+            while (i1 < g)
+            {
+                i2 += v;
+                i1++;
+            }
+
+            Console.WriteLine("Количество квадратов = {0}" , i2);
+            Console.ReadKey();
+        }
+    }
+
+}
